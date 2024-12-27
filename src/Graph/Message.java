c36085cb307e2b1a524b197cc3811ef4a3808da4
oren1levy <oren.levy10@gmail.com>
2024-12-27 12:45:09 +0200

    build Message

--- src/Graph/Message.java
+++ src/Graph/Message.java
@@ -8,5 +8,26 @@ public class Message {
     public final double asDouble;
     public final Date date;
 
+    public  Message(String asText){
+        this.asText = asText;
+        double parseDouble;
+        try {
+            parseDouble = Double.parseDouble(asText);
+        } catch (NumberFormatException e) {
+            parseDouble = Double.NaN;
+        }
+        this.asDouble = parseDouble;
+        this.data = asText.getBytes();
+        this.date = new Date();
+    }
+
+    public Message(double asDouble) {
+        this(Double.toString(asDouble));
+    }
+
+    public Message(byte[] data) {
+        this(new String(data));
+    }
+
 
 }
