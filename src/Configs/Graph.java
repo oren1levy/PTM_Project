bdc11962f0df4082a6432b2ae8d8c5a6502ae274
oren1levy <oren.levy10@gmail.com>
2025-01-13 16:19:24 +0200

    final updates for milestone3

--- src/Configs/Graph.java
+++ src/Configs/Graph.java
@@ -24,12 +24,10 @@ public class Graph extends ArrayList<Node> {
         TopicManagerSingleton.TopicManager topicManager = TopicManagerSingleton.get();
         Map<String, Node> nodeMap = new HashMap<>();
 
-        // יצירת קודקודים מכל ה-Topics
         for (String topicName : topicManager.getTopics().keySet()) {
             nodeMap.putIfAbsent("T" + topicName, new Node("T" + topicName));
         }
 
-        // הוספת קודקודים מתוך Agents
         for (Topic topic : topicManager.getTopics().values()) {
             for (Agent agent : topic.getPubs()) {
                 String agentName = "A" + agent.getName();
@@ -37,14 +35,12 @@ public class Graph extends ArrayList<Node> {
             }
         }
 
-        // יצירת הקשתות (edges) בין הקודקודים
         for (Topic topic : topicManager.getTopics().values()) {
             Node topicNode = nodeMap.get("T" + topic.name);
             if (topicNode == null) {
                 throw new NullPointerException("Topic node for " + topic.name + " is null");
             }
 
-            // הוספת קשתות מסוכנים מפרסמים
             for (Agent agent : topic.getPubs()) {
                 Node agentNode = nodeMap.get("A" + agent.getName());
                 if (agentNode != null) {
@@ -52,7 +48,6 @@ public class Graph extends ArrayList<Node> {
                 }
             }
 
-            // הוספת קשתות מסוכנים מנויים
             for (Agent agent : topic.getSubs()) {
                 Node agentNode = nodeMap.get("A" + agent.getName());
                 if (agentNode != null) {
@@ -61,7 +56,6 @@ public class Graph extends ArrayList<Node> {
             }
         }
 
-        // הוספת הקודקודים לרשימת הגרף
         nodes.addAll(nodeMap.values());
     }
 
